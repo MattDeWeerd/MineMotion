2f0076c1040ccfd6108c9aef1793528d4e967b42
MattDeWeerd <matt.deweerd43@gmail.com>
2024-04-16 22:26:16 -0400

    Update Base URL

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite';
-import {PrimeVueResolver} from 'unplugin-vue-components/resolvers';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import { PrimeVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/MineMotion/",
   plugins: [
     vue(),
     Components({
-      resolvers: [
-        PrimeVueResolver()
-      ]
-    })]
-})
+      resolvers: [PrimeVueResolver()],
+    }),
+  ],
+});
